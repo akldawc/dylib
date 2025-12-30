df6637ea42dac64008a03c0c9ced063ddf35774f
akldawc <115430041+akldawc@users.noreply.github.com>
2025-12-30 14:24:10 +0800

    更新 mylib.m

--- src/mylib.m
+++ src/mylib.m
@@ -1,78 +1,55 @@
 #import "mylib.h"
-#import <UIKit/UIKit.h>
+#import <Cocoa/Cocoa.h>
 
-// 悬浮窗容器
-static UIView *verifyFloatView = nil;
-// 输入框
-static UITextField *keyInputTF = nil;
+static NSWindow *verifyWin = nil;
 
-// 模拟验证按钮点击
-void verifyKeyAction(UIButton *btn) {
-    NSString *inputKey = keyInputTF.text?:@"";
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"验证结果"
-                                                                   message:inputKey.length>0?
-                                                                   [NSString stringWithFormat:@"卡密「%@」模拟验证成功！",inputKey]:
-                                                                   @"请输入卡密后验证！"
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
-    // 获取当前顶层控制器
-    UIViewController *topVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-    while (topVC.presentedViewController) {
-        topVC = topVC.presentedViewController;
-    }
-    [topVC presentViewController:alert animated:YES completion:nil];
-}
+void showKeyVerifyWindow() {
+    [NSApplication sharedApplication];
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
 
-// 创建悬浮窗
-void showKeyVerifyWindow(UIViewController *vc) {
-    // 避免重复创建
-    if (verifyFloatView) {
-        [verifyFloatView removeFromSuperview];
-        verifyFloatView = nil;
-    }
-    
-    // 悬浮窗主视图
-    verifyFloatView = [[UIView alloc] initWithFrame:CGRectMake(50, 200, 300, 150)];
-    verifyFloatView.backgroundColor = [UIColor whiteColor];
-    verifyFloatView.layer.cornerRadius = 10;
-    verifyFloatView.layer.shadowColor = [UIColor blackColor].CGColor;
-    verifyFloatView.layer.shadowOpacity = 0.3;
-    verifyFloatView.layer.shadowOffset = CGSizeMake(2, 2);
-    
-    // 标题标签
-    UILabel *titleLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, 300, 20)];
-    titleLab.text = @"卡密验证";
-    titleLab.textAlignment = NSTextAlignmentCenter;
-    titleLab.font = [UIFont boldSystemFontOfSize:16];
-    [verifyFloatView addSubview:titleLab];
-    
+    // 创建悬浮窗
+    verifyWin = [[NSWindow alloc] initWithContentRect:NSMakeRect(200, 200, 300, 150)
+                                           styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable
+                                             backing:NSBackingStoreBuffered
+                                               defer:NO];
+    [verifyWin setTitle:@"卡密验证"];
+    [verifyWin setLevel:NSFloatingWindowLevel];
+    [verifyWin center];
+
+    // 内容视图
+    NSView *content = [verifyWin contentView];
+
+    // 标题
+    NSTextField *title = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 110, 300, 20)];
+    title.stringValue = @"卡密验证";
+    title.editable = NO;
+    title.bordered = NO;
+    title.alignment = NSTextAlignmentCenter;
+    [content addSubview:title];
+
     // 输入框
-    keyInputTF = [[UITextField alloc] initWithFrame:CGRectMake(20, 50, 260, 30)];
-    keyInputTF.placeholder = @"请输入卡密";
-    keyInputTF.borderStyle = UITextBorderStyleRoundedRect;
-    keyInputTF.font = [UIFont systemFontOfSize:14];
-    [verifyFloatView addSubview:keyInputTF];
-    
+    NSTextField *tf = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 70, 260, 24)];
+    tf.placeholderString = @"请输入卡密";
+    [content addSubview:tf];
+
     // 验证按钮
-    UIButton *verifyBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    verifyBtn.frame = CGRectMake(80, 95, 140, 30);
-    [verifyBtn setTitle:@"模拟验证卡密" forState:UIControlStateNormal];
-    [verifyBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [verifyBtn setBackgroundColor:[UIColor systemBlueColor]];
-    verifyBtn.layer.cornerRadius = 5;
-    [verifyBtn addTarget:self action:@selector(verifyKeyAction:) forControlEvents:UIControlEventTouchUpInside];
-    [verifyFloatView addSubview:verifyBtn];
-    
-    // 添加到控制器视图
-    [vc.view addSubview:verifyFloatView];
-    // 开启拖拽（可选）
-    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panFloatView:)];
-    [verifyFloatView addGestureRecognizer:pan];
+    NSButton *btn = [[NSButton alloc] initWithFrame:NSMakeRect(100, 30, 100, 30)];
+    btn.title = @"模拟验证";
+    btn.bezelStyle = NSBezelStyleRounded;
+    [btn setTarget:self];
+    [btn setAction:@selector(verify:)];
+    [content addSubview:btn];
+
+    [verifyWin makeKeyAndOrderFront:nil];
+    [NSApp activateIgnoringOtherApps:YES];
 }
 
-// 悬浮窗拖拽逻辑
-void panFloatView(UIPanGestureRecognizer *pan) {
-    CGPoint trans = [pan translationInView:verifyFloatView.superview];
-    verifyFloatView.center = CGPointMake(verifyFloatView.center.x + trans.x, verifyFloatView.center.y + trans.y);
-    [pan setTranslation:CGPointZero inView:verifyFloatView.superview];
+void verify:(NSButton *)btn {
+    NSTextField *tf = (NSTextField *)[[btn superview] viewWithTag:101];
+    NSString *key = tf.stringValue?:@"";
+    
+    NSAlert *alert = [[NSAlert alloc] init];
+    alert.messageText = @"验证结果";
+    alert.informativeText = key.length > 0 ? [NSString stringWithFormat:@"卡密「%@」验证成功（模拟）", key] : @"请输入卡密！";
+    [alert runModal];
 }
